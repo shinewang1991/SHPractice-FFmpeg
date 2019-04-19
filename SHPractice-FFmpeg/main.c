963c6c3f2782dfa149049b747ddc0d9088082d4f
wangxiang <wangxiang@yixia.com>
2019-04-19 16:29:23 +0800

    Initial Commit

---
+++ SHPractice-FFmpeg/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SHPractice-FFmpeg
+//
+//  Created by Shine on 2019/4/19.
+//  Copyright © 2019 shine. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
