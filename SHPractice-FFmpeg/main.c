10d653ab3d58b6bebad7fe46326a5e94833c4e0e
wangxiang <wangxiang@yixia.com>
2019-04-19 17:13:50 +0800

    initial commit

--- SHPractice-FFmpeg/main.c
+++ SHPractice-FFmpeg/main.c
@@ -7,9 +7,20 @@
 //
 
 #include <stdio.h>
+#include <libavutil/log.h>
+#include <libavformat/avformat.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int ret;
+    av_log_set_level(AV_LOG_INFO);
+    AVFormatContext *fmt_ctx = NULL;
+    ret = avformat_open_input(&fmt_ctx, "/Users/shine/iOS学习练习/SHPractice-FFmpeg/SHPractice-FFmpeg/test.mp4", NULL , NULL);
+    if(ret < 0){
+        av_log(NULL, AV_LOG_ERROR, "Can't open file \n");
+    }
+    
+    av_dump_format(fmt_ctx, 0, "./test.mp4", 0);
+    avformat_close_input(&fmt_ctx);
     return 0;
 }
