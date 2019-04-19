65b8de9966ffd14c3e5c74de5e03ab56f01d26e5
wangxiang <wangxiang@yixia.com>
2019-04-19 17:18:39 +0800

    打印多媒体信息

--- SHPractice-FFmpeg/main.c
+++ SHPractice-FFmpeg/main.c
@@ -10,10 +10,13 @@
 #include <libavutil/log.h>
 #include <libavformat/avformat.h>
 
-int main(int argc, const char * argv[]) {
-    
+
+//打印对媒体信息
+void demo1(){
     int ret;
     av_log_set_level(AV_LOG_INFO);
+    
+    //    av_register_all();  //'av_register_all' is deprecated. 4.0以后不需要了
     AVFormatContext *fmt_ctx = NULL;
     ret = avformat_open_input(&fmt_ctx, "/Users/shine/iOS学习练习/SHPractice-FFmpeg/SHPractice-FFmpeg/test.mp4", NULL , NULL);
     if(ret < 0){
@@ -22,5 +25,9 @@ int main(int argc, const char * argv[]) {
     
     av_dump_format(fmt_ctx, 0, "./test.mp4", 0);
     avformat_close_input(&fmt_ctx);
+}
+
+int main(int argc, const char * argv[]) {
+    demo1();
     return 0;
 }
